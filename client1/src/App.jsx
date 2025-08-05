5faf1581daf6c79377666df243ff155aba164c9c
Zulfiqar-Ali <zulfiqarnizar60@gmail.com>
2025-08-05 14:09:40 +0500

    changes commited

---
+++ client1/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./ui/Header"
+import Footer from "./ui/Footer"
+import Container from "./ui/Container"
+
+function App() {
+
+  return (
+    <main>
+      <Header/>
+      <Container>
+        <p>Ecom Dude</p>
+      </Container>
+      <Footer/>
+    </main>
+  )
+}
+
+export default App
