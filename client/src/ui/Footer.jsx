f47fa6f93635604c00343205f5e4667558f2f123
Zulfiqar-Ali <zulfiqarnizar60@gmail.com>
2025-08-11 01:01:29 +0500

    footer done till now

--- client/src/ui/Footer.jsx
+++ client/src/ui/Footer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Container from './Container.jsx'; // Assuming you have a Container component
+import payment from '../../assets/payment.webp'; // Adjust the path as necessary
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+   <div className='mt-10'>
+  <Container className="flex flex-col md:flex-row items-center gap-4 justify-between">
+    <p>@2025 Ecommerce Guru. All rights reserved.</p>
+    <img src={payment} alt="payment-img" className='object-cover'/>
+  </Container>
+</div>
   )
 }
 
