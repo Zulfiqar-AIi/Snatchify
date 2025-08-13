d0d2c3302f4cebd9f5363aa64356a620d7f1a5ef
Zulfiqar-Ali <zulfiqarnizar60@gmail.com>
2025-08-14 03:30:45 +0500

    complex changes

--- admin/index.mjs
+++ admin/index.mjs
@@ -1,11 +1,24 @@
 import express from 'express'
 const app = express()
 import "dotenv/config"
+import cors from 'cors'
+import { fileURLToPath } from 'url'
+import path from 'path'
+import { readdirSync } from 'fs'
 
 const port = process.env.PORT || 8000
+app.use(cors())
+app.use(express.json())
+
+const __filename = fileURLToPath(import.meta.url) 
+const __dirname = path.dirname(__filename)
+
+const routesPath = path.resolve(__dirname, "./routes")
+const routeFiles = readdirSync(routesPath);
+console.log("Loading routes from:", routeFiles)
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, "public", "index.html"))
 })
 
 app.listen(port, () => {
