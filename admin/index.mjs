80333c20d0af3d2acdd4c4ba0e54afc6bbe2a569
Zulfiqar-Ali <zulfiqarnizar60@gmail.com>
2025-08-13 18:25:22 +0500

    admin created till now

---
+++ admin/index.mjs
@@ -0,0 +1,13 @@
+import express from 'express'
+const app = express()
+import "dotenv/config"
+
+const port = process.env.PORT || 8000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
